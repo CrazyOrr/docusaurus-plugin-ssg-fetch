d28837161218207fa10f4c5b624c1d09a3a64b6f
Lei Wang <wanglei021212@gmail.com>
2025-11-17 21:46:30 +0800

    ignore ts-jest warning about `isolatedModules`

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,15 @@ import { createDefaultPreset } from "ts-jest";
 /** @type {import('jest').Config} */
 export default {
   ...createDefaultPreset(),
+  // workaround for https://github.com/kulshekhar/ts-jest/issues/5013
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        diagnostics: {
+          ignoreCodes: [151002],
+        },
+      },
+    ],
+  },
 };
